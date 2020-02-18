af20c3ddf472cb60c6351fcebc69a6bd8129b8ca
Anwesh43 <anweshthecool0@gmail.com>
2020-02-19 05:23:46 +0530

    adding constants and libraries

--- takelinehorizontallyview/src/main/java/com/anwesh/uiprojects/takelinehorizontallyview/TakeLineHorizontallyView.kt
+++ takelinehorizontallyview/src/main/java/com/anwesh/uiprojects/takelinehorizontallyview/TakeLineHorizontallyView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.takelinehorizontallyview
 /**
  * Created by anweshmishra on 19/02/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
