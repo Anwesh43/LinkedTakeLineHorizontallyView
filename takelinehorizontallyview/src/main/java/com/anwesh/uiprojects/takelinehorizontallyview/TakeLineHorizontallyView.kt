9417b0b4ab4ac627629f562800e2128e37a07c94
Anwesh43 <anweshthecool0@gmail.com>
2020-02-19 05:55:39 +0530

    addng TakeLineHorizontallyView class

--- takelinehorizontallyview/src/main/java/com/anwesh/uiprojects/takelinehorizontallyview/TakeLineHorizontallyView.kt
+++ takelinehorizontallyview/src/main/java/com/anwesh/uiprojects/takelinehorizontallyview/TakeLineHorizontallyView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawTLHNode(i : Int, scale : Float, paint : Paint) {
     drawTakeLinesHorizontally(scale, w, paint)
     restore()
 }
+
+class TakeLineHorizontallyView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
